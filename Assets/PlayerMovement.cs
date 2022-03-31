d383cbf285eac0c8930700aa356b66dcf060053d
JBushRSGC <102624850+JBushRSGC@users.noreply.github.com>
2022-03-31 13:47:08 -0400

    Simple Character and Animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,16 +7,24 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 0.5f;
     public float jumpForce = 2f;
     public bool grounded = false;
+    //for now, set this to zero. Easy to imagine setting this by script
     public Vector3 respawnPoint = Vector3.zero;
 
+    //to control animations
+    public Animator playerAnimator;
 
-    public Rigidbody rigidbody;
+    //to control physics (gravity, forces)
+    public Rigidbody thisRigidbody;
+
+    public Transform cameraTransform;
 
 
     void Start(){
-        rigidbody = GetComponent<Rigidbody>();    
+        thisRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
+    //to be called by things that kill the player
     public void RespawnPlayer()
     {
         transform.position = respawnPoint;
@@ -25,28 +33,49 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
+        //shoot a ray downwards. If it hits something, raycast is true
+        grounded = Physics.Raycast(transform.position, Vector3.down, 2f);
 
-        grounded = Physics.Raycast(transform.position, Vector3.down, 1.0f);
+        //which direction we move. If zero, stationary
+        Vector3 movementDirection = Vector3.zero;
 
+        //input controls
         if (Input.GetKey("w"))
         {
-            transform.position += Vector3.forward * speed;
+            movementDirection += cameraTransform.forward;
         }
         if (Input.GetKey("s"))
         {
-            transform.position += Vector3.back * speed;
+            movementDirection += -cameraTransform.forward; //back
         }
         if (Input.GetKey("a"))
         {
-            transform.position += Vector3.left * speed;
+            movementDirection += -cameraTransform.right; //left
         }
         if (Input.GetKey("d"))
         {
-            transform.position += Vector3.right * speed;
+            movementDirection += cameraTransform.right;
         }
+
+        //if we are moving
+        if (movementDirection.sqrMagnitude > 0)
+        {
+            //transform the player's position
+            transform.position += movementDirection.normalized * speed;
+            //tell our animator we're running
+            playerAnimator.SetBool("IsRunning", true);
+            //rotate towards wherever we're looking
+            transform.rotation = Quaternion.LookRotation(movementDirection);
+        }
+        //otherwise we're standing still
+        else
+        {
+            playerAnimator.SetBool("IsRunning", false);
+        }
+        //jump
         if (Input.GetKeyDown("space") && grounded)
         {
-            rigidbody.AddForce(Vector3.up * jumpForce);
+            thisRigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
 }
